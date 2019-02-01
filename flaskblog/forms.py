58bfb3074b01e89f98291da99982d52e3d4abb5b
B.W. Block <bbwilliam3@gmail.com>
2019-01-31 18:55:41 -0700

    login working

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 from flaskblog.models import User
@@ -8,6 +8,8 @@ from flaskblog.models import User
 class LoginForm(Form):
     username = StringField(u'Username', validators=[DataRequired()])
     password = PasswordField(u'Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
 
     def validate(self):
         check_validate = super(LoginForm, self).validate()
