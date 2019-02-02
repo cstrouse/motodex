f2ad06c37a3a3b6bc57a0648ac1d6128f2fec9a3
B.W. Block <bbwilliam3@gmail.com>
2019-02-01 22:23:03 -0700

    working

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import Form
+from flask.ext.login import current_user
 from flask_wtf.file import FileField, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
@@ -59,3 +60,17 @@ class UpdateAccountForm(Form):
                         validators=[DataRequired(), Email()])
     picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
     submit = SubmitField('Update')
+
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('That email is taken. Please choose a different one.')
+
