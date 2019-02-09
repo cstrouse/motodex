aaae458895ec32be562bdfdfab7afce14da1aa71
B.W. Block <bbwilliam3@gmail.com>
2019-02-08 19:00:43 -0700

    prod

---
+++ flaskblog/settings.py
@@ -0,0 +1,43 @@
+import tempfile
+db_file = tempfile.NamedTemporaryFile()
+mail_username = 'bwblock3@gmail.com'
+mail_password = 'test'
+
+class Config(object):
+    SECRET_KEY = 'uGEg7L8XpP4'
+
+
+class ProdConfig(Config):
+    ENV = 'prod'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+
+    CACHE_TYPE = 'simple'
+
+
+class DevConfig(Config):
+    ENV = 'dev'
+    DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///../database.db'
+
+    CACHE_TYPE = 'null'
+    ASSETS_DEBUG = True
+
+    # config mail server
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = mail_username
+    MAIL_PASSWORD = mail_password
+
+class TestConfig(Config):
+    ENV = 'test'
+    DEBUG = True
+    DEBUG_TB_INTERCEPT_REDIRECTS = False
+
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_file.name
+    SQLALCHEMY_ECHO = True
+
+    CACHE_TYPE = 'null'
+WTF_CSRF_ENABLED = False
