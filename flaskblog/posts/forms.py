6e01a6684761e2809537504f38fb12fa38001ce5
BW Block <bwblock3@gmail.com>
2020-05-23 18:18:18 -0600

    fix url bug

--- flaskblog/posts/forms.py
+++ flaskblog/posts/forms.py
@@ -8,13 +8,13 @@ from flaskblog.models import Category
 class PostForm(Form):
     category = SelectField('Category', coerce=int, validators=[DataRequired()])
     title = StringField('Title', validators=[DataRequired(), Length(min=2, max=50)])
-    link = StringField('Link to Listing (Allowed sites - ebaymotors, craigslist)', validators=[DataRequired(), Length(min=10, max=30)])
+    link = StringField('Link to Listing (Allowed sites - Ebaymotors, Craigslist)', validators=[DataRequired(), Length(min=10, max=150)])
     content = TextAreaField('Description', validators=[DataRequired()])
     submit = SubmitField('Post')
     
     def validate_link(self, link):
         allowed = ['craigslist', 'ebaymotors']
-        prefixes = ["http://", "https://"]
+        prefixes = ["http://", "https:/"]
         if link.data[:7] not in prefixes:
             raise ValidationError("That is an invalid prefix")
         for s in allowed:
